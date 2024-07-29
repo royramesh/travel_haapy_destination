bc185f86d8ab265d349e63b58a0031740002290d
Saikat Das <140289625+X-Saikat-93@users.noreply.github.com>
2024-07-29 17:52:41 +0530

    ghvyvfyjtgfvyjtgv
    mhgm

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,27 @@ $( document ).ready(function() {
 })
 
 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000, // Adjust for faster/slower image changes
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
+
 $( document ).ready(function() {
 
     $('.owl-carousel').owlCarousel({
